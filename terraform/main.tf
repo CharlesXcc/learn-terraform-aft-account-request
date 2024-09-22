87ddb479433bee2ed147ead0cfdac34e50982211
CharlesXcc <charles.xcc@gmail.com>
2024-09-22 13:03:55 -0400

    Update main.tf
    added sandbox enviorment

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    AccountEmail              = "charles.xu@topnotchtech.cc"
+    AccountName               = "sandbox"
+    ManagedOrganizationalUnit = "EngineeringPlatform"
+    SSOUserEmail              = "charles.xu@topnotchtech.cc"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
